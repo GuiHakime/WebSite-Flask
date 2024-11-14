f97e5a6579e05f810495244e679d7028b2979c9c
GuiHakime <guihakimeandrade@gmail.com>
2024-11-14 10:54:41 -0300

    Estilização da página contatos

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class ContactForm(FlaskForm):
-    name = StringField('name', validators = [DataRequired(), Length(min = 2, max = 50)])
+    name = StringField('Nome', validators = [DataRequired(), Length(min = 2, max = 50)])
     email = StringField("Email", validators = [DataRequired(), Email()])
     mensagem = TextAreaField("Mensagem", validators = [DataRequired(), Length(min = 5, max = 500)])
     submit = SubmitField("Enviar")
